797c2d936306029a29291fd4744227a4e8ce320d
neocheekiong <neocheekiong@gmail.com>
2020-06-13 14:14:49 +0800

    outlined a schema of user data.

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+    'env': {
+        'commonjs': true,
+        'es6': true,
+        'node': true,
+        'jquery': true,
+        'jest': true,
+    },
+    'extends': 'eslint:recommended',
+    'globals': {
+        'Atomics': 'readonly',
+        'SharedArrayBuffer': 'readonly',
+        'window': true
+    },
+    'parserOptions': {
+        'ecmaVersion': 2018
+    },
+    'rules': {
+        'indent': ['warn', 4],
+        'linebreak-style': ['error', 'windows'],
+        'quotes': ['warn', 'single'],
+        'semi': ['warn', 'always'],
+        'object-property-newline': ['warn', { allowAllPropertiesOnSameLine: true }],
+        'comma-dangle': ['warn', {
+            'arrays': 'only-multiline',
+            'objects': 'only-multiline',
+            'imports': 'only-multiline',
+            'exports': 'only-multiline',
+        }],
+        'space-before-function-paren': 'warn',
+        'object-curly-spacing': ["warn", 'always']
+    }
+};
--- app.js
+++ app.js
@@ -1,14 +1,94 @@
 const express = require('express');
 const app = express();
-const port = process.env.port || 3000
-app.set('view engine', 'ejs')
+const port = process.env.port || 3000;
+app.set('view engine', 'ejs');
 
-function renderFrontPage(request, response) {
-    response.render("index")
+function renderFrontPage (request, response) {
+    response.render('index');
 }
 
-app.get('/', renderFrontPage)
+app.get('/', renderFrontPage);
 
 app.listen(port, () => {
-    console.log("Listening on port ", port)
-})
+    console.log('Listening on port ', port);
+});
+
+Person = {
+    name: String,
+    emailAddress: String,
+    phoneNumber: {
+        areaCode: Number,
+        number: Number
+    },
+    address: {
+        street: String,
+        postalCode: Number,
+        cityTown: String,
+        country: String,
+        state: String
+    },
+    placeOfPractice: {
+        nameOfPractice: String,
+        address: {
+            street: String,
+            postalCode: Number,
+            cityTown: String,
+            country: String,
+            state: String
+        }
+    },
+    formalEducation: [{
+        type: String,
+        major: String,
+        institute: String,
+        yearAttained: Number,
+        certificate: File,
+        verified: Boolean
+    }],
+    continuingEducation: [{
+        nameOfCourse: String,
+        numberOfHours: Number,
+        certificate: File,
+        dateAttended: Date,
+        verified: Boolean
+    }],
+    supervisionHours: [{
+        supervisor: {
+            name: String,
+            email: String,
+            phoneNumber: {
+                areaCode: Number,
+                number: Number
+            }
+        },
+        numberOfHours: Number,
+        documentation: File,
+        verified: Boolean
+    }],
+    workExperience: [{
+        organisation: {
+            name:String,
+            address: {
+                street: String,
+                postalCode: Number,
+                cityTown: String,
+                country: String,
+                state: String
+            }
+        },
+        startDate: Date,
+        endDate: Date,
+        roles: String,
+        documentation: File,
+        verified: Boolean
+    }]
+    // tests: [{
+    //     testType: String,
+    //     testDate: Date,
+    //     testScore: Number,
+    //     test: [{
+    //         questionNumber: Number,
+    //         correct: Boolean
+    //     }]
+    // }]
+};
