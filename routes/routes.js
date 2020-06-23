c56602f87ef693311b2cd7d2f4ddd9ba17fa361c
neocheekiong <neocheekiong@gmail.com>
2020-06-23 23:09:01 +0800

    personal details page

--- routes/routes.js
+++ routes/routes.js
@@ -12,5 +12,8 @@ module.exports = (app) => {
     app.get('/', controllers.renderPage(views.INDEX_PAGE)({ user }));
     app.get('/register', controllers.renderPage(views.REGISTRATION_PAGE)());
     app.post('/register', controllers.processRegistration);
+    app.get('/personal/:userid', controllers.renderPage(views.PERSONAL_INFO_PAGE)({ user }));
+    app.post('/personal/:userid', controllers.updateInfo(views.PERSONAL_INFO_PAGE)({ user }));
     app.get('/education/:userid', controllers.renderPage(views.EDUCATION_PAGE)({ user }));
+    app.post('/education/:userid', controllers.renderPage(views.EDUCATION_PAGE)({ user }));
 };
--- views/views.js
+++ views/views.js
@@ -2,5 +2,6 @@ module.exports = {
     INDEX_PAGE: 'index.ejs',
     REGISTRATION_PAGE: 'registration.ejs',
     ERROR_PAGE: 'error.ejs',
-    EDUCATION_PAGE: 'education.ejs'
+    EDUCATION_PAGE: 'education.ejs',
+    PERSONAL_INFO_PAGE: 'personal-info.ejs'
 };
