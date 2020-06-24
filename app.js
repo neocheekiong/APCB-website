982ef53fcf8581d935654ca6f212ae563416d5cc
neocheekiong <neocheekiong@gmail.com>
2020-06-24 16:23:42 +0800

    UPDATED PORT VAR

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
  */
 const express = require('express');
 const app = express();
-const PORT = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
